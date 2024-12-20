8f27430fdebb890177aa6dc8a03dbbfe030f8a3f
Emil <hustlehustlese@gmail.com>
2024-12-21 01:08:46 +0300

    fetching data from json and place into front

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -2,7 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
     fetch('api/test.php')
         .then(response => response.json())
         .then(data => {
+            const accontInfo = document.querySelector('.account-info');
+            const accountName = document.createElement('div');
+            accountName.className = 'account-name';
+            accountName.innerText = data.name;
+            accontInfo.appendChild(accountName);
             console.log(data);
         })
         .catch(error => console.error(error));
 });
+
--- src/index.php
+++ src/index.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title>Получение JSON-данных</title>
+    <link rel="stylesheet" href="/assets/css/main.css">
 </head>
 <body>
+    <div class="account-info"></div>
     <script src="assets/js/script.js" defer></script>
 </body>
 </html>
